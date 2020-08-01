265382e429431f580c6e49d4b1dc04233a8b8b9d
Mithun Halder <mithun.tutorial.one@gmail.com>
2020-08-01 17:28:11 +0600

    darkmode & lightmode vuex intregated

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,17 @@
   </div>
 </template>
 
+<script>
+export default {
+  mounted() {
+    const isDarkMode = this.$store.getters.isDarkMode;
+
+    document.body.style.background = isDarkMode ? "#212c4f" : "#f0f3f5";
+  },
+};
+</script>
+
 <style lang="scss">
-@import "@/global-styles/colors.scss";
-@import "@/global-styles/typography.scss";
 body {
   background-color: $dark-blue;
 }
--- src/views/SignInFlow/SignIn.vue
+++ src/views/SignInFlow/SignIn.vue
@@ -1,19 +1,38 @@
 <template>
-  <div class="container">
-    <div class="request">
+  <div
+    class="container"
+    :class="{ 'light-background': !isDarkMode, 'dark-backgrond': isDarkMode }"
+  >
+    <div
+      class="request"
+      :class="{ 'dark-request': !isDarkMode, 'light-request': isDarkMode }"
+    >
       Don't have a mithun account?
       <router-link to="request">Request an account</router-link>
     </div>
     <div class="login">
-      <img src="@/assets/default.png" alt="" />
-      <h4>Sign into Mithun</h4>
-      <input type="email" placeholder="Email" /><input
+      <img src="@/assets/light-logo.png" alt="" v-show="!isDarkMode" />
+      <img src="@/assets/dark-logo.png" alt="" v-show="isDarkMode" />
+      <h4 :class="{ 'dark-text': !isDarkMode, 'light-text': isDarkMode }">
+        Sign into Mithun
+      </h4>
+      <input
+        type="email"
+        placeholder="Email"
+        :class="{ 'dark-field': !isDarkMode, 'light-field': isDarkMode }"
+      /><input
         type="password"
         placeholder="Password"
+        :class="{ 'dark-field': !isDarkMode, 'light-field': isDarkMode }"
       />
       <button>Sign In</button>
 
-      <router-link to="recover">Forgot your password?</router-link>
+      <router-link
+        to="recover"
+        :class="{ 'dark-link': !isDarkMode, 'light-link': isDarkMode }"
+        >Forgot your password?</router-link
+      >
+      <button @click="toggleDarkMode" type="button">Toggle</button>
     </div>
   </div>
 </template>
@@ -21,10 +40,57 @@
 <script>
 export default {
   name: "SignIn",
+  data() {
+    return {
+      //   isDarkMode: true,
+    };
+  },
+  computed: {
+    isDarkMode() {
+      return this.$store.getters.isDarkMode;
+    },
+  },
+  methods: {
+    toggleDarkMode() {
+      this.$store.dispatch("triggerDarkMode");
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.light-background {
+  background-color: $light-grey;
+}
+.dark-background {
+  background-color: $dark-blue;
+}
+
+.light-text {
+  color: $white;
+}
+.dark-text {
+  color: $black;
+}
+
+.light-field {
+  background: rgba(255, 255, 255, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.3);
+  }
+}
+
+.dark-field {
+  background: rgba(198, 208, 235, 0.2);
+  border: 1px solid rgba(0, 0, 0, 0.2);
+
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.3);
+  }
+}
+
 .container {
   display: flex;
 
@@ -36,10 +102,19 @@ export default {
   position: absolute;
   top: 40px;
   right: 40px;
+}
+
+.light-request {
   color: rgba(255, 255, 255, 0.3);
 
   a {
-    color: white;
+    color: $white;
+  }
+}
+.dark-request {
+  color: rgba(0, 0, 0, 0.3);
+  a {
+    color: $black;
   }
 }
 
@@ -84,12 +159,23 @@ button {
   border: none;
   margin-top: 20px;
   margin-bottom: 40px;
+  &:focus {
+    border: none;
+    outline: none;
+  }
 }
 a {
   line-height: 25px;
   font-size: 16px;
   text-align: center;
-  color: rgba(255, 255, 255, 0.3);
   text-decoration: none;
+}
+
+.light-link {
+  color: rgba(255, 255, 255, 0.3);
+}
+
+.dark-link {
+  color: rgba(0, 0, 0, 0.3);
 }
 </style>
